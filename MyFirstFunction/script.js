e1353ad25600fbb1efc02025c1f18fbb2dd51d27
Pavel <pavel.kodehode@gmail.com>
2024-10-28 10:47:39 +0100

    Added Grid Tablet and My First Function

---
+++ MyFirstFunction/script.js
@@ -0,0 +1,14 @@
+let Name = "Pavel"
+let Surname = "Nekrasov"
+console.log(Name,Surname);
+
+
+
+let Numb1 = 4;
+let Numb2 = 20;
+let Total;
+total = sum(Numb1,Numb2);
+function sum (Numb1,Numb2){
+  return Numb1 + Numb2;
+}
+console.log (Total);
