1784c3ee388fea1cbfef621e026904cf2a606164
Pavel <pavel.kodehode@gmail.com>
2024-10-29 10:40:39 +0100

    Add Task 7

--- MyFirstFunction/script.js
+++ MyFirstFunction/script.js
@@ -12,3 +12,61 @@ function sum (Numb1,Numb2){
   return Numb1 + Numb2;
 }
 console.log (Total);
+
+
+// let og const som nøkkelordå opprette variabler 
+// const forNavn = "Pavel";
+// const alder = 18.9;
+// const dato = "01/01/1999"
+
+
+// Det er foskjell på å skrive ut  verdien av en variabel og
+// en tekst.
+// console.log ("forNavn");
+// console.log (forNavn);
+// console.log (forNavn);
+
+// forNavn = "Per"
+
+
+
+// Datatyper;
+// string = text
+// number = tall - Javascript bruker dette !!!
+// integer / int= haltall
+// float = desimaltall
+// boolean = true/false
+// bit = 0 eller 1
+// byte = 8 bit
+// date = dato
+
+// console.log(typeof forNavn + " - forNavn");
+// console.log(forNavn);
+// console.log(typeof alder + " - alder");
+// console.log(alder);
+// console.log(typeof dato);
+
+
+//  Aritmetiske Operatorer
+// ----------------------------------------------------
+// + - * / % er operatorer 
+// Brøk 1 
+// ----
+//   4   = 1/4
+// = Tilordningsoperator
+// +=, -=, *=, /=, %= Eks: alder += 1; => alder = alder + 1;
+ 
+let navn = "John";
+let alder = 17;
+let alder2; //deklarasjon
+// let sumAlder = 0;
+alder2 = 10;
+// sumAlder = alder / alder2; 
+alder = alder + alder2;
+alder += alder2; // En hurtigere måte å skrive linje ovenfor på.
+alder2 = alder2 / 0;
+
+//console.log(alder);
+// console.log(alder +1);
+console.log(alder2);
+// console.log(sumAlder);
